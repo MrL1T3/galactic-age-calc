9d95e6c464ea34fa4baa5e55e2e86b4473f24181
Emil Anton <emilbstn@gmail.com>
2022-12-22 17:40:20 -0800

    add second test to check if function works on decimal inputs

--- __tests__/mercury.tests.js
+++ __tests__/mercury.tests.js
@@ -1,9 +1,12 @@
 import Mercury from "../src/mercury.js";
 
 describe('Mercury', () => {
-
     test('should return one Earth year in Mercury years', () => {
       const earthToMercury = new Mercury(1);
       expect(earthToMercury).mercYears === 0.24;
+    })
+    test('should return two and a half Earth years in Mercury years', () => {
+      const earthToMercury = new Mercury(2.5);
+      expect(earthToMercury).mercYears === 0.48;
     })
 })
