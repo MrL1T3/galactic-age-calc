8bf2ee971d6cc44bd6779417d2a0e121109b2ff8
Emil Anton <emilbstn@gmail.com>
2022-12-24 21:11:40 -0800

    remove old console log used in verifying jest functionality

--- src/mercury.js
+++ src/mercury.js
@@ -1,6 +1,5 @@
 export default function Mercury (earthAge, previousAge, comingAge){
   this.mercYears = (earthAge / 0.24).toFixed(2);
-  console.log("this happened");
   this.mercYearsString = earthAge + ' Earth years is equal to ' + this.mercYears + ' Mercury years.'
   this.mercAge = 'You are ' + this.mercYears + ' Mercury years old.';
   this.mercYearsSince = ((earthAge - previousAge) / 0.24).toFixed(2);
