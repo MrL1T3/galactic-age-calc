4640db910cee0821060b7f9dbaabd4b9ec77da7a
Emil Anton <emilbstn@gmail.com>
2022-12-24 19:32:17 -0800

    fix a typo in second test statement

--- __tests__/mercury.tests.js
+++ __tests__/mercury.tests.js
@@ -7,6 +7,10 @@ describe('Mercury', () => {
     })
     test('should return two and a half Earth years in Mercury years', () => {
       const earthToMercury = new Mercury(2.5);
-      expect(earthToMercury).mercYears === 0.48;
+      expect(earthToMercury).mercYears === 0.60;
+    })
+    test('should return a string saying the input Earth years and its equivalent in Mercury Years', () => {
+      const earthToMercury = new Mercury(1);
+      expect(earthToMercury).mercYearsString === '1 Earth years is equal to 0.24 Mercury Years';
     })
 })
