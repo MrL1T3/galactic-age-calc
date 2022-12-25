415222b1f6a41ed1bc5f4bd6405310c266c757dc
Emil Anton <emilbstn@gmail.com>
2022-12-24 20:10:29 -0800

    fix mercYearString property to pass functioning test

--- src/mercury.js
+++ src/mercury.js
@@ -1,4 +1,5 @@
 export default function Mercury (earthYears){
-  this.mercYears = (earthYears * 0.24)
-  this.mercYearsString = earthYears + ' Earth years is equal to ' + ' Mercury years.'
+  this.mercYears = (earthYears * 0.24);
+  console.log("this happened");
+  this.mercYearsString = earthYears + ' Earth years is equal to ' + this.mercYears + ' Mercury years.'
 }
