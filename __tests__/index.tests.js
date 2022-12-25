3c089a56e1aa80fae5e385a64d8d84b6b04f67b3
Emil Anton <emilbstn@gmail.com>
2022-12-25 10:28:46 -0800

    add test for all five times since past age

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -13,10 +13,14 @@ describe('spaceCalc', () => {
         expect(spaceCalc(20, 10, 35)).toEqual(['You are 83.33 Mercury years old.','You are 32.26 Venus years old.','You are 10.64 Mars years old.','You are 1.69 Jupiter years old.']);
     }),
     test.skip("should return how long its been on earth in an array", () => {
-        expect(spaceCalc(20, 10, 35)).toContain('10 Earth years have passed.')
+        expect(spaceCalc(20, 10, 35)).toContain('10 Earth years have passed.');
     }),
-    test("should return how long its been on mercury in an array", () => {
-        expect(spaceCalc(20, 10, 35)).toContain('41.67 Mercury years have passed.')
+    test.skip("should return how long its been on mercury in an array", () => {
+        expect(spaceCalc(20, 10, 35)).toContain('41.67 Mercury years have passed.');
+    }),
+    test('should return all years since in a single array', () => {
+        expect(spaceCalc(20, 10, 35)).toEqual(['10 Earth years have passed.','41.67 Mercury years have passed.','16.13 Venus years have passed.','5.32 Mars years have passed.','0.84 Jupiter years have passed.']);
     })
+
 });
 
