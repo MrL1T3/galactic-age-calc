b4855dbf2f1b3a0b025d8007257f13fe5c2c07b0
Emil Anton <emilbstn@gmail.com>
2022-12-24 21:46:08 -0800

    add test of yearsOld function to return mercury years

--- __tests__/planetYears.tests.js
+++ __tests__/planetYears.tests.js
@@ -1,5 +1,8 @@
 import planetYears from "../src/planetYears";
 
-describe('planetYears', () => {
-    
+describe('planetYears.yearsOld', () => {
+    test('should return the users input earth age in mercury years', () => {
+        const mercConvert = new planetYears(20, 10, 35);
+        expect(mercConvert.yearsOld).toBe('83.33')
+    })
 })
