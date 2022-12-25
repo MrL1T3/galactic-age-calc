f55340fd54b6b0dcfa36fb46b31c9dc6e3547b17
Emil Anton <emilbstn@gmail.com>
2022-12-25 10:04:16 -0800

    add test for receiving all four converted ages from spaceCalc

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,13 +1,16 @@
-import planetYears from '../src/planetYears';
 import spaceCalc from "../src/index.js";
 
 describe('spaceCalc', () => {
-    test('should return the input earthAge in jupiter years', () => {
+    test.skip('should return the input earthAge in jupiter years', () => {
         expect(spaceCalc(20, 10, 35)).toContain('You are 1.69 Jupiter years old.')
-    }).skip,
-    test('should return mercYearsOld and jupiterYearsOld results together in an array', () => {
+    }),
+    test.skip('should return mercYearsOld and jupiterYearsOld results together in an array', () => {
         console.log(spaceCalc(20, 10, 35));
-        expect(spaceCalc(20, 10, 35)).toEqual(['You are 83.33 Mercury years old.','You are 1.69 Jupiter years old.']);
+        expect(spaceCalc(20, 10, 35)).toContain(['You are 83.33 Mercury years old.','You are 1.69 Jupiter years old.']);
+    }),
+    test('should return all results together in an array', () => {
+        console.log(spaceCalc(20, 10, 35));
+        expect(spaceCalc(20, 10, 35)).toEqual(['You are 83.33 Mercury years old.','You are 32.26 Venus years old.','You are 10.64 Mars years old.','You are 1.69 Jupiter years old.']);
     })
 });
 
