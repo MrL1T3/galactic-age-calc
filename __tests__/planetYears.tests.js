c9a87efa4241ad223dfcf4b30b7c8470106cbd67
Emil Anton <emilbstn@gmail.com>
2022-12-24 22:42:09 -0800

    add test for venusYearsOld function

--- __tests__/planetYears.tests.js
+++ __tests__/planetYears.tests.js
@@ -8,5 +8,12 @@ describe('planetYears.mercYearsOld', () => {
     test('should return a sentence describing the users age on mercury', () => {
         const mercConvert = new planetYears(0.24, 0.62, 1.88, 11.86);
         expect(mercConvert.mercYearsOld(20)).toBe('You are 83.33 Mercury years old.')
-    })
-})
+    });
+});
+
+describe('planetYears.venusYearsOld', () => {
+    test('should return a sentence stating the input age in venus years', () => {
+        const venusConvert = new planetYears(0.24, 0.62, 1.88, 11.86);
+        expect(venusConvert.venusYearsold(20)).toBe('You are 32.26 Venus years old.');
+    });
+});
