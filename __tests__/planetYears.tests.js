cd531ba6f8339b35a5171565bdc7f9e952c7f84c
Emil Anton <emilbstn@gmail.com>
2022-12-24 22:21:13 -0800

    change name of function from yearsOld to mercYearsOld

--- __tests__/planetYears.tests.js
+++ __tests__/planetYears.tests.js
@@ -1,12 +1,12 @@
 import planetYears from "../src/planetYears";
 
-describe('planetYears.yearsOld', () => {
+describe('planetYears.mercYearsOld', () => {
     // test('should return the users input earth age in mercury years', () => {
     //     const mercConvert = new planetYears(0.24, 0.62, 1.88, 11.86);
-    //     expect(mercConvert.yearsOld(20)).toBe('83.33')
+    //     expect(mercConvert.mercYearsOld(20)).toBe('83.33')
     // })
     test('should return a sentence describing the users age on mercury', () => {
         const mercConvert = new planetYears(0.24, 0.62, 1.88, 11.86);
-        expect(mercConvert.yearsOld(20)).toBe('You are 83.33 Mercury years old.')
+        expect(mercConvert.mercYearsOld(20)).toBe('You are 83.33 Mercury years old.')
     })
 })
--- src/planetYears.js
+++ src/planetYears.js
@@ -1,7 +1,13 @@
 export default class planetYears{
-    constructor(earthAge, previousAge, futureAge){
-        this.earthAge = earthAge;
-        this.previousAge = previousAge;
-        this.futureAge = futureAge;
+    constructor(mercYearConvert, venusYearConvert, marsYearConvert, jupiterYearconvert){
+        this.mercYearConvert = mercYearConvert;
+        this.venusYearConvert = venusYearConvert;
+        this.marsYearConvert = marsYearConvert;
+        this.jupiterYearconvert = jupiterYearconvert;
+    }
+    mercYearsOld(earthAge) {
+        const mercAge = (earthAge / this.mercYearConvert).toFixed(2);
+        const mercAgeString = 'You are ' + mercAge + ' Mercury years old.'
+        return mercAgeString;
     }
 }
