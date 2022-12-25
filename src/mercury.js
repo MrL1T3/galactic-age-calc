e90efa6e94ff099e0f856c18fd21ba47351c1242
Emil Anton <emilbstn@gmail.com>
2022-12-25 11:09:33 -0800

    removed unused and uneeded mercury.js files

--- src/mercury.js
+++
@@ -1,8 +0,0 @@
-export default function Mercury (earthAge, previousAge, comingAge){
-  this.mercYears = (earthAge / 0.24).toFixed(2);
-  this.mercYearsString = earthAge + ' Earth years is equal to ' + this.mercYears + ' Mercury years.'
-  this.mercAge = 'You are ' + this.mercYears + ' Mercury years old.';
-  this.mercYearsSince = ((earthAge - previousAge) / 0.24).toFixed(2);
-  this.mercYearsSinceString = 'It has been ' + ((earthAge - previousAge) / 0.24).toFixed(2) + ' Mercury years since you were that old.'
-  this.mercYearsUntil = ((comingAge - earthAge) / 0.24).toFixed(2);
-}
