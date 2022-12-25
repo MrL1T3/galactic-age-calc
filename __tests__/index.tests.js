ce6f3755f6095977549445704701d515cae5601a
Emil Anton <emilbstn@gmail.com>
2022-12-24 23:54:39 -0800

    add first test for index.js business logic

---
+++ __tests__/index.tests.js
@@ -0,0 +1,8 @@
+import planetYears from '../src/planetYears';
+import spaceCalc from "../src/index.js";
+
+describe('spaceCalc', () => {
+    test('should return the input earthAge in mercury years', () => {
+        expect(spaceCalc(20, 10, 35)).toBe('You are 83.33 Mercury years old.')
+    });
+});
