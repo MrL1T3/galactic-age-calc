0ac82d53f85f2d00c1c175ffe67209b035017837
Emil Anton <emilbstn@gmail.com>
2022-12-25 10:24:56 -0800

    reformat __yearsOld functions into single push statement

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ export default function spaceCalc(earthAge, previousAge, futureAge) {
     const spaceYears = new planetYears (earthAge, previousAge, futureAge);
     let ageArray = [];
     let timeSinceArray =[];
-    ageArray.push(spaceYears.mercYearsOld(earthAge));
-    ageArray.push(spaceYears.venusYearsOld(earthAge));
-    ageArray.push(spaceYears.marsYearsOld(earthAge));
-    ageArray.push(spaceYears.jupiterYearsOld(earthAge));
+    ageArray.push(spaceYears.mercYearsOld(earthAge),spaceYears.venusYearsOld(earthAge),spaceYears.marsYearsOld(earthAge),spaceYears.jupiterYearsOld(earthAge));
     console.log(ageArray);
     let result = spaceYears.yearsSince(10);
     timeSinceArray.push(result.earthYears, result.mercuryYears);
