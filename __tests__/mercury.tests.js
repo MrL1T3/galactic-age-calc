c36eb4254cd3f35a475f7c09483f5acf8788632b
Emil Anton <emilbstn@gmail.com>
2022-12-24 21:28:47 -0800

    fix minor typos in test statements

--- __tests__/mercury.tests.js
+++ __tests__/mercury.tests.js
@@ -1,5 +1,4 @@
 import Mercury from "../src/mercury.js";
-
 describe('Mercury', () => {
     test('should return one Earth year in Mercury years', () => {
       const earthToMercury = new Mercury(1);
