b3afa321db806c204f1243cbbf91d225f01666b1
Emil Anton <emilbstn@gmail.com>
2022-12-25 09:46:03 -0800

    add test for returning multiple results from spaceCalc

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -2,10 +2,12 @@ import planetYears from '../src/planetYears';
 import spaceCalc from "../src/index.js";
 
 describe('spaceCalc', () => {
-    // test('should return the input earthAge in mercury years', () => {
-    //     expect(spaceCalc(20, 10, 35)).toBe('You are 83.33 Mercury years old.')
-    // }),
     test('should return the input earthAge in jupiter years', () => {
         expect(spaceCalc(20, 10, 35)).toBe('You are 1.69 Jupiter years old.')
+    }).skip,
+    test('should return mercYearsOld and jupiterYearsOld results together in an array', () => {
+        console.log(spaceCalc(20, 10, 35));
+        expect(spaceCalc(20, 10, 35)).toBe(['You are 83.33 Mercury years old', 'You are 1.69 Jupiter years old']);
     })
 });
+
