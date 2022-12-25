ecb15ce696ff3f50e7b4669a1372b17aeff426ff
Emil Anton <emilbstn@gmail.com>
2022-12-25 11:28:20 -0800

    add test for using yearsTill in spaceCalc

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -18,9 +18,11 @@ describe('spaceCalc', () => {
     test.skip("should return how long its been on mercury in an array", () => {
         expect(spaceCalc(20, 10, 35)).toContain('41.67 Mercury years have passed.');
     }),
-    test('should return all years since in a single array', () => {
+    test.skip('should return all years since in a single array', () => {
         expect(spaceCalc(20, 10, 35)).toEqual(['10 Earth years have passed.','41.67 Mercury years have passed.','16.13 Venus years have passed.','5.32 Mars years have passed.','0.84 Jupiter years have passed.']);
+    }),
+    test('should return all times until the projected age', () => {
+        expect(spaceCalc(15, 10, 20)).toEqual(['It will be 5 Earth years until you are that old.','It will be 20.83 Mercury years until you are that old.','It will be 8.06 Venus years until you are that old.','It will be 2.66 Mars years until you are that old.','It will be 0.42 Jupiter years until you are that old.']);
     })
-
 });
 
