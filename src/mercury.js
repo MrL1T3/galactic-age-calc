5dc8c4162762c97a3fe9a6baf09e1095d0a99903
Emil Anton <emilbstn@gmail.com>
2022-12-24 20:42:29 -0800

    add mercYearsSince property to pass test

--- src/mercury.js
+++ src/mercury.js
@@ -1,6 +1,7 @@
-export default function Mercury (earthYears){
-  this.mercYears = (earthYears * 0.24);
+export default function Mercury (earthAge, previousAge){
+  this.mercYears = (earthAge * 0.24);
   console.log("this happened");
-  this.mercYearsString = earthYears + ' Earth years is equal to ' + this.mercYears + ' Mercury years.'
+  this.mercYearsString = earthAge + ' Earth years is equal to ' + this.mercYears + ' Mercury years.'
   this.mercAge = 'You are ' + this.mercYears.toFixed(1) + ' Mercury years old.';
+  this.mercYearsSince = ((earthAge - previousAge) * 0.24).toFixed(1)
 }
