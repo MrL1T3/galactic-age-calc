6ed229a00a4fc1f6c75660064c3774f18799754b
Emil Anton <emilbstn@gmail.com>
2022-12-24 22:08:39 -0800

    add test for producing a sentence from yearsOld

--- __tests__/planetYears.tests.js
+++ __tests__/planetYears.tests.js
@@ -2,7 +2,11 @@ import planetYears from "../src/planetYears";
 
 describe('planetYears.yearsOld', () => {
     test('should return the users input earth age in mercury years', () => {
-        const mercConvert = new planetYears(20, 10, 35);
+        const mercConvert = new planetYears(0.24, 0.62, 1.88, 11.86);
         expect(mercConvert.yearsOld(20)).toBe('83.33')
+    })
+    test('should return a sentence describing the users age on mercury', () => {
+        const mercConvert = new planetYears(0.24, 0.62, 1.88, 11.86);
+        expect(mercConvert.yearsOld).toBe('You are 83.33 Mercury years old.')
     })
 })
