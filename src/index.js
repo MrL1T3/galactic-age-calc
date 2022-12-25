b1c0016bc75a3514c10ef4a05e13afb9599df699
Emil Anton <emilbstn@gmail.com>
2022-12-25 10:06:24 -0800

    add other ange conversion functions to spaceCalc

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ export default function spaceCalc(earthAge, previousAge, futureAge) {
     const spaceYears = new planetYears (earthAge, previousAge, futureAge);
     let ageArray = [];
     ageArray.push(spaceYears.mercYearsOld(earthAge));
+    ageArray.push(spaceYears.venusYearsOld(earthAge));
+    ageArray.push(spaceYears.marsYearsOld(earthAge));
     ageArray.push(spaceYears.jupiterYearsOld(earthAge));
     console.log(ageArray);
     return ageArray;
