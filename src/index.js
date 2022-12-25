9fcc8f84796653d5cf8836cad42af840bcfded50
Emil Anton <emilbstn@gmail.com>
2022-12-24 23:56:47 -0800

    fill spaceCalc function to pass first test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import '../css/styles.css';
+import planetYears from './planetYears';
+
+export default function spaceCalc(earthAge, previousAge, futureAge) {
+    const spaceYears = new planetYears (earthAge, previousAge, futureAge);
+    return spaceYears.mercYearsOld();
+}
