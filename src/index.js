bac1944c232a0ab6849d78a18c2dbd932a11f2a9
Emil Anton <emilbstn@gmail.com>
2022-12-25 11:33:22 -0800

    add use of yearsTill to spaceCalc

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import planetYears from './planetYears';
 export default function spaceCalc(earthAge, previousAge, futureAge) {
     const spaceYears = new planetYears (earthAge, previousAge, futureAge);
     let ageArray = [];
-    let timeSinceArray =[];
+    let timeSinceArray = [];
+    let timeTillArray = [];
     ageArray.push(spaceYears.mercYearsOld(earthAge),spaceYears.venusYearsOld(earthAge),spaceYears.marsYearsOld(earthAge),spaceYears.jupiterYearsOld(earthAge));
     console.log(ageArray);
-    let result = spaceYears.yearsSince(10);
-    timeSinceArray.push(result.earthYears, result.mercuryYears, result.venusYears, result.marsYears, result.jupiterYears);
+    let pastResult = spaceYears.yearsSince(previousAge);
+    timeSinceArray.push(pastResult.earthYears, pastResult.mercuryYears, pastResult.venusYears, pastResult.marsYears, pastResult.jupiterYears);
     console.log(timeSinceArray);
-    return timeSinceArray;
+    let futureResult = spaceYears.yearsTill(futureAge);
+    timeTillArray.push(futureResult.earthYears, futureResult.mercuryYears, futureResult.venusYears, futureResult.marsYears, futureResult.jupiterYears);
+    console.log(timeTillArray)
+    return timeTillArray;
 }
