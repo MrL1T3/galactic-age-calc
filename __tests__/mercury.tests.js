9707d8000f96c0456f5a8e5696a78001522e614b
Emil Anton <emilbstn@gmail.com>
2022-12-24 20:09:37 -0800

    fix syntactic misunderstanding of expect() method that caused false positives

--- __tests__/mercury.tests.js
+++ __tests__/mercury.tests.js
@@ -3,14 +3,15 @@ import Mercury from "../src/mercury.js";
 describe('Mercury', () => {
     test('should return one Earth year in Mercury years', () => {
       const earthToMercury = new Mercury(1);
-      expect(earthToMercury).mercYears === 0.24;
+      expect(earthToMercury.mercYears).toBe(0.24);
     })
     test('should return two and a half Earth years in Mercury years', () => {
       const earthToMercury = new Mercury(2.5);
-      expect(earthToMercury).mercYears === 0.60;
+      expect(earthToMercury.mercYears).toBe(0.60);
     })
-    test('should return a string saying the input Earth years and its equivalent in Mercury Years', () => {
+    test('should return a string saying the input Earth years and its equivalent in Mercury years.', () => {
       const earthToMercury = new Mercury(1);
-      expect(earthToMercury).mercYearsString === '1 Earth years is equal to 0.24 Mercury Years';
+      console.log("bad stuff");
+      expect(earthToMercury.mercYearsString).toBe('1 Earth years is equal to 0.24 Mercury years.');
     })
 })
